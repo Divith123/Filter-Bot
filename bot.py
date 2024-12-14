f94923d5296e95214dba9a0692a7c6036f2020b6
Divith S <divithselvam23@gmail.com>
2024-12-14 21:39:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # Initialize Pyrogram client
     app = pyrogram.Client(
         "filter_bot",
-        bot_token=Config.TG_BOT_TOKEN,
+        bot_token=Config.TELEGRAM_BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
